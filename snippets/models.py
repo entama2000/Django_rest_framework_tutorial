538a10eccd2f2655e3ad9251b094c3d8372ca4bf
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-08-22 20:45:32 -0400

    fix bug

--- snippets/models.py
+++ snippets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Snippet(models.MOdel):
+class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
